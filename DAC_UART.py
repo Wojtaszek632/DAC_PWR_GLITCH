2f48e5989b2bfdd263b3fde39b99d6b441b7bf63
Wojciech-Windows <wojciechmanczakpoczta@gmail.com>
2022-12-18 21:58:31 +0100

    Deleted debug response frames

--- DAC_UART.py
+++ DAC_UART.py
@@ -9,19 +9,10 @@ args = int(sys.argv[1])
 ser = serial.Serial('COM6',115200,timeout=5) #'/dev/ttyUSB0'
 ser.close()
 ser.open()
-print(args)
+
 strings = str(args) + '\n'
-print(strings)
 ser.write(bytes(strings,'UTF-8'))
-print(ser.readline())  # read a '\n' terminated line
-print(ser.readline())  # read a '\n' terminated line
-print(ser.readline())  # read a '\n' terminated line
-print(ser.readline())  # read a '\n' terminated line
 
-#ser.write(lowByte)
-#print(ser.readline()) 
-
-#print(ser.readline()) 
 ser.close()
     
 
